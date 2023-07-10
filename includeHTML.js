0766abcb701c31ddac86d7f72dd2272391f40dc0
willachui <59659651+willachui@users.noreply.github.com>
2023-07-09 17:55:17 -0700

    Create includeHTML.js

---
+++ includeHTML.js
@@ -0,0 +1,27 @@
+function includeHTML(cb) {
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        file = elmnt.getAttribute("INCLUDE-HTML");
+        if (file) {
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        elmnt.innerHTML = this.responseText;
+                    }
+                    if (this.status == 404) {
+                        elmnt.innerHTML = "Page not found.";
+                    }
+                    elmnt.removeAttribute("INCLUDE-HTML");
+                    includeHTML(cb);
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return;
+        }
+    }
+    if (cb) cb();
+};
